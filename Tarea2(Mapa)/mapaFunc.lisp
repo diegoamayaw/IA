09ac5f7e010c911c3a9fe3e5babdaf3054137788
diegoamayaw <diegoamayaw@gmail.com>
2018-03-17 18:30:17 -0600

    agrego funciones probadas

--- Tarea2(Mapa)/mapaFunc.lisp
+++ Tarea2(Mapa)/mapaFunc.lisp
@@ -1,4 +1,6 @@
 
+(load "~/Documents/ITAM/IA/Tarea2(Mapa)/datos.lisp")
+
 (defun AStar (nodoInicio nodoFinal)
 	(setq abierto nil)
 	(setq cerrado nil)
@@ -7,18 +9,19 @@
 		(cond ((null abierto)(return-from AStar 'No_se_encontro_solucion)))
 		))
 
-;;Se crean las tablas hash con un tamaño inicial de 50
-(defun creaTablas()
-	(setq BdCoord (make-hash-table :size 50) BdHijos (make-hash-table :size 50) ))
-
+;;Fija los nombres de origen y destino en variables nomIni y nomDest
+(defun setNames (n1 n2)
+	(setq nomIni n1 nomDest n2))
 ;;Obtiene coordenadas de la tabla hash y las guarda en una variable global.
 (defun getCoordIniFin(nomIni nomFin)
 	(setq coordIni (gethash nomIni BdCoord) coordFin (gethash nomFin BdCoord)))
+(defun toRadianes (cantidad)
+	(return-from toRadianes (/ (* cantidad 3.141592) 180)))
 
-;;Calcula h(n) distancia en línea recta usando la fórmula de Haversine
+;;Calcula h(n) distancia en línea recta en km usando la fórmula de Haversine
 (defun calculaHn(nodo)
 	(setq coordNode (gethash (car nodo) BdCoord))
-	(setq lat1 (car coordFin) lat2 (car coordNode) long1 (cadr coordFin) long2 (cadr coordNode))
+	(setq lat1 (toRadianes (car coordFin)) lat2 (toRadianes (car coordNode)) long1 (toRadianes (cadr coordFin)) long2 (toRadianes (cadr coordNode)))
 	(setq RadT 6371 difLat (- lat2 lat1) difLong (- long2 long1))
 	(setq a (+ (expt (sin (/ difLat 2)) 2) (* (cos lat1) (cos lat2) (expt (sin (/ difLong 2)) 2))))
 	(setq dist (* 2 RadT (asin (expt a 0.5))))
@@ -48,20 +51,33 @@
 	(return-from getNombre (car nodo)))
 
 ;;Obtiene el nodoInfo del padre de nodo
-(defun getPadre(nodo lst)
+(defun buscaPadre(nodo lst)
 	(setq nomPadre (second nodo))
-	(cond ((null lst) 0)
-		  ((equal nomPadre (second (car lst)))(return-from getPadre (car lst)))
+	(cond ((null lst))
+		  ((equal nomPadre (first (car lst)))(return-from buscaPadre (car lst)))
 	(t (getPadre nodo (cdr lst)))))
 
 ;;Obtiene la ruta encontrada desde el final al inicio siguiendo la referencia de los padres
-(defun defRoute(nodo)
-	(setq rutaFinal nil)
-	(defRoute2 nodo))
+(defun defRoute(nodo cerrado)
+	(setq rutaFinal '())
+	(push nomDest rutaFinal)
+	(push (second nodo) rutaFinal)
+	(defRoute2 nodo cerrado))
 
-(defun defRoute2 (nodo)
-	(cond ((equal (car (first nodo)) nomInicio)0)
-	(t(setq vater (getPadre nodo))
-	  (push (car (second vater)) rutaFinal)
-	  (defRoute2 vater))))
+(defun defRoute2 (nodo cerrado)
+	(cond ((equal (first (car cerrado)) nomIni)rutaFinal)
+	(t(setq vater (buscaPadre nodo cerrado))
+	  (push (second vater) rutaFinal)
+	  (defRoute2 vater (cdr cerrado)))))
+
+;__________________________________________________________PRUEBAS______________________________________________________________________
+
+(setNames 'A 'D)
+(getCoordIniFin nomIni nomDest)
+(setq cerrado '())
+(push (creaInfoNodo 'A NIL 0 0) cerrado)
+(push (creaInfoNodo 'B 'A 0 0) cerrado)
+(push (creaInfoNodo 'K 'B 0 0) cerrado)
+(creaInfoNodo 'D 'K 0 0)
+
 
